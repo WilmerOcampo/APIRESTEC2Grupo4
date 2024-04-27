5f40e22bdbc76f2dbd2e3a57dcb0628a43ac300c
Wilmer Ocampo Quispe <Wilmer_Ocampo@hotmail.com>
2024-04-27 11:36:40 -0500

    Subiendo Examen API REST Grupo4

---
+++ src/main/java/com/grupo4/apirest/ApiRestEc2Grupo4Application.java
@@ -0,0 +1,13 @@
+package com.grupo4.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestEc2Grupo4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestEc2Grupo4Application.class, args);
+    }
+
+}
